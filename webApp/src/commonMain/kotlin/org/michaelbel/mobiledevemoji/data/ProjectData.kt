9eed1b69fc565047815d7fca12e69bf757333c17
michaelbel <michael-bel@outlook.com>
2024-08-27 12:58:49 +0300

    Update project

--- webApp/src/commonMain/kotlin/org/michaelbel/mobiledevemoji/data/ProjectData.kt
+++ webApp/src/commonMain/kotlin/org/michaelbel/mobiledevemoji/data/ProjectData.kt
@@ -7,4 +7,4 @@ const val TELEGRAM_PACK_1 = "https://t.me/addstickers/MobileDevEmojiTgs"
 const val TELEGRAM_PACK_2 = "https://t.me/addstickers/MobileDevEmojiTgs2"
 const val FIGMA_URL = "https://www.figma.com/community/file/1385339470177359146"
 const val PACK_1_SIZE = 200
-const val PACK_2_SIZE = 8
+const val PACK_2_SIZE = 16
