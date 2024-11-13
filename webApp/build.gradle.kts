a64f6a578422fa1fc48e808e0c1bed0daeadc61b
michaelbel <michael-bel@outlook.com>
2024-11-13 10:47:55 +0300

    Update project

--- webApp/build.gradle.kts
+++ webApp/build.gradle.kts
@@ -5,8 +5,8 @@ import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.compose)
 }
 
